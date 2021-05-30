820a34afb3461a254735fd426eaea1d15d57fb45
John Watson <42574977+johnwatson484@users.noreply.github.com>
2021-05-30 16:27:55 +0100

    Add start script (#5)

--- app/routes/validate.js
+++ app/routes/validate.js
@@ -6,6 +6,7 @@ module.exports = [{
   options: {
     handler: async (request, h) => {
       await delay()
+      console.info('Agreement validated')
       return h.response({
         sbi: request.payload?.sbi,
         isValid: true
