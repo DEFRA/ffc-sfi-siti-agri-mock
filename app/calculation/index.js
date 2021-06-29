bbcc4e85baddd56eed3693c7887a78f743898653
steve-dickinson <steve.dickinson@defra.gov.uk>
2021-06-29 07:41:19 +0100

    update calculation

--- app/calculation/index.js
+++ app/calculation/index.js
@@ -1,17 +1,17 @@
 const buildResponse = (calculationPayload) => {
-  calculate(calculationPayload, 'soilProtection')
-  calculate(calculationPayload, 'permanentGrasslandProtection')
-  calculate(calculationPayload, 'moorlandGrazing')
-  calculate(calculationPayload, 'livestockWelfare')
+  calculate(calculationPayload, 'soilProtection', 6)
+  calculate(calculationPayload, 'permanentGrasslandProtection', 6)
+  calculate(calculationPayload, 'moorlandGrazing', 6)
+  calculate(calculationPayload, 'livestockWelfare', 6)
   return calculationPayload
 }
 
-const calculate = (calculationPayload, standardType) => {
+const calculate = (calculationPayload, standardType, amount) => {
   const standard = calculationPayload.calculation.standards[standardType].actions
   for (const [key, value] of Object.entries(standard)) {
     standard[key] = value.map(action => {
-      action.expression = 'x*y'
-      action.value = 616800
+      action.expression = `AREA*${amount}`
+      action.value = action.area * amount
       return action
     })
   }
